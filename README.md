779734c1e94f03200965d3dfdca1be4d2f464951
Cristian Vasilache <greyfocus@users.noreply.github.com>
2015-05-06 23:12:54 +0300

    Removed unused methods and general code cleanup.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Binary Clock
+
+A binary clock is a clock that displays time in binary format. If you're a true
+geek, you probably already have one - if not, be sure to clone the repo and play
+with it.
+
+For more details, you can also check the article about the [binary
+clock][http://en.wikipedia.org/wiki/Binary_clock] from Wikipedia.
+
--- src/js/binary-clock.js
+++ src/js/binary-clock.js
@@ -1,59 +1,65 @@
 jQuery(document).ready(function($) {
     var clockDiv = $(".binary-clock");
 
-    function displayDigit(number, digit) {
-
-        for (var i=1; i <= 4; i++) {
-            console.log(number);
-            if (number & 1 == 1) {
-                light(digit, i);
+    /**
+     * Displays a single digit. 
+     *
+     * @param digit the digit to be displayed.
+     * @param column the column.
+     */
+    function displayDigit(digit, column) {
+        for (var i = 1; i <= 4; i++) {
+            if (digit & 1 == 1) {
+                light(column, i);
             }
             else {
-                dim(digit, i);
+                dim(column, i);
             }
 
-            number = number >> 1;
+            digit = digit >> 1;
         }
-
     }
 
-    function light(digit, index) {
-        var led = $(clockDiv.find(".binary-clock-led-" + digit + "-" + index));
+    /**
+     * Lights up the "led" on the given position.
+     * 
+     * @param column the column of the led.
+     * @param row the row of the led.
+     */
+    function light(column, row) {
+        var led = $(clockDiv.find(".binary-clock-led-" + column + "-" + row));
 
-        console.log(led);
-        console.log("light " + digit + ", " + index + led);
         led.addClass("lighted");
     }
 
-    function dim(digit, index) {
-        var led = $(clockDiv.find(".binary-clock-led-" + digit + "-" + index));
-        console.log(led);
-        console.log("dim " + digit + ", " + index + led);
+    /**
+     * Dims the "led" on the given position.
+     * @param column the column of the led.
+     * @param row the row of the led.
+     */
+    function dim(column, row) {
+        var led = $(clockDiv.find(".binary-clock-led-" + column + "-" + row));
+        
         led.removeClass("lighted");
     }
 
-    function displaySeconds() {
-        var date = new Date();
-        var seconds = date.getSeconds().toString();
-
-        displayDigit(parseInt(seconds[seconds.length-1]), 2);
-        if (seconds.length > 1) {
-            displayDigit(parseInt(seconds[seconds.length-2]), 1);
-        }
-
-        setTimeout(function() {
-            displaySeconds();
-        }, 1000);
-    }
-
-    function displayNumber(number, digit) {
+    /**
+     * Displays a two digit number.
+     *
+     * @param number the number to be displayed.
+     * @param position the position (column) where the first digit should be displayed.
+     */
+    function displayNumber(number, position) {
         var str = number.toString();
-        displayDigit(parseInt(str[str.length - 1]), digit + 1);
+        displayDigit(parseInt(str[str.length - 1]), position + 1);
         if (str.length > 1) {
-            displayDigit(parseInt(str[str.length - 2]), digit);
+            displayDigit(parseInt(str[str.length - 2]), position);
         }
     }
 
+    /**
+     * Displays time with periodic updates.
+     */
     function displayTime() {
         var date = new Date();
         displayNumber(date.getSeconds(), 5);
